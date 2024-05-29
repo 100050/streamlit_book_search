f95c4532bbba0537a28a0a9fd748778a2d781e8b
100050 <82698917+100050@users.noreply.github.com>
2024-05-29 16:29:50 +0900

    Create dall-e.py

---
+++ pages/dall-e.py
@@ -0,0 +1,18 @@
+import urllib.request
+from PIL import Image
+
+@st.cache_data
+def func(prompt):
+    client = OpenAI(api_key=key)
+    response = client.images.generate(model="dall-e-3",prompt=prompt)
+    image_url = response.data[0].url
+    urllib.request.urlretrieve(image_url, 'img.png')
+    img = Image.open("img.png")
+    st.image(img, use_column_width=True)
+
+st.session_state.key = st.text_input("key", type="password")
+st.header("그림 그리기")
+prompt = st.text_input("프롬포트")
+
+if st.button("그리기"):
+    func(prompt)
